6846443c0cf72f62aab7ad9eca43e36abe31ca98
David Sobrinho Camurça <david.camurcaa@gmail.com>
2016-09-08 00:24:37 -0300

    new effect website

--- jquery.particleground.js
+++ jquery.particleground.js
@@ -379,13 +379,13 @@
     maxSpeedY: 0.7,
     directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
-    density: 10000, // How many particles will be generated: one particle every n pixels
+    density: 3000, // How many particles will be generated: one particle every n pixels
     dotColor: '#666666',
     lineColor: '#666666',
     particleRadius: 7, // Dot size
     lineWidth: 1,
     curvedLines: false,
-    proximity: 100, // How close two dots need to be before they join
+    proximity: 70, // How close two dots need to be before they join
     parallax: true,
     parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
     onInit: function() {},
